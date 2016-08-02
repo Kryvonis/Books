156c40dacd95e82a0ae425e84c6fabdba833e9e4
Kryvonis <kryvonis.artem@gmail.com>
2016-08-02 13:27:28 +0300

    try to do docker

--- src/test_books.py
+++ src/test_books.py
@@ -7,50 +7,31 @@ from celery import Celery
 from flask import Flask, render_template, request, session, abort
 from flask_mail import Mail, Message
 
+from config import config
+
 app = Flask(__name__)
+app.config.from_object(config['development'])
+# config['development'].init_app(app)
 
-# EMAIL SETTINGS
-EMAIL_SERVER = 'smtp.gmail.com'
-EMAIL_PORT = 465
-# CELERY_SETTINGS
-CELERY_BROKER_URL = ''  # your brocker url for celery
-CELERY_RESULT_BACKEND = ''  # your backend for storing task results
-# EMAIL SENDER SETTINGS
-MAIL_USERNAME = ''  # your email username
-MAIL_PASSWORD = ''  # your email pass
-# LOG FILE SRC
-LOG_FILE = ''  # where stored your log file
-
-# URL FOR ELASTICSEARCH
-ES_URL = 'http://localhost:9200'
-app.config.update(
-    DEBUG=False,
-    MAIL_SERVER=EMAIL_SERVER,
-    MAIL_PORT=EMAIL_PORT,
-    MAIL_USE_SSL=True,
-    CELERY_BROKER_URL=CELERY_BROKER_URL,
-    CELERY_RESULT_BACKEND=CELERY_RESULT_BACKEND,
-    MAIL_USERNAME=MAIL_USERNAME,
-    MAIL_PASSWORD=MAIL_PASSWORD
-)
-# variable for sending Email
+# variable for sending Emaill
 mail = Mail(app)
 
 
 # prepearing celery for flask app
 def make_celery(app):
-    celery = Celery('TestBooks', backend=app.config['CELERY_RESULT_BACKEND'],
+    celery = Celery('test_books', backend=app.config['CELERY_RESULT_BACKEND'],
                     broker=app.config['CELERY_BROKER_URL'], )
 
     celery.conf.update(app.config)
-    TaskBase = celery.Task
 
-    class ContextTask(TaskBase):
+    # TaskBase =
+
+    class ContextTask(celery.Task):
         abstract = True
 
         def __call__(self, *args, **kwargs):
             with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
+                return super().__call__(*args, **kwargs)
 
     celery.Task = ContextTask
     return celery
@@ -66,6 +47,8 @@ def index():
     Index page
     :return rendered page in html:
     """
+
+    app.logger.warning('AFTER REQUEST')
     return render_template('TestBooks/index.html')
 
 
@@ -81,18 +64,18 @@ def search_run():
     try:
         json = request.json
         session['email'] = json['email']
-        search_text.delay(json['email'], json['searchText'])
+        search_text(json['email'], json['searchText'])
     except:
-        abort(400)
+        abort(404)
     return "", 200
 
 
 @celery.task()
-def search_text(email, search_text):
+def search_text(email, searching_text):
     """
     Method where we do text search by using elasticsearch
     :param email: email where send result of searching
-    :param search_text: what we search
+    :param searching_text: what we search
     :return: result of searching
     """
     # create request data for elasticsearch
@@ -100,25 +83,25 @@ def search_text(email, search_text):
         "_source": False,
         "query": {
             "match_phrase": {
-                "text": search_text
+                "text": searching_text
             }
         }
     }
+
     request_data = dumps(request_data)
     # count time for searching
     start_time = time.time()
 
-    result_search = 'Result by searching with word: "{}"\n'.format(search_text)
+    result_search = 'Result by searching with word: "{}"\n'.format(searching_text)
     # send search request with data
-    res = requests.get('{0}/books/_search?pretty'.format(ES_URL), data=request_data).json()
-
+    res = requests.get('{0}/books/_search?pretty'.format(app.config['ES_URL']), data=request_data).json()
     # parsing response from request
     if res['hits']['total']:  # checking if we have some result
         try:
             for hit in res['hits']['hits']:
                 book_id = hit['_id']
                 book_name = hit['_type']
-                data_book = requests.get('{0}/books/{1}/{2}?pretty'.format(ES_URL, book_name, book_id),
+                data_book = requests.get('{0}/books/{1}/{2}?pretty'.format(app.config['ES_URL'], book_name, book_id),
                                          data=request_data).json()
                 book_chapter = data_book['_source']['chapter']
                 book_page = data_book['_id']
@@ -135,7 +118,7 @@ def search_text(email, search_text):
     end_time = time.time() - start_time
     # get logger for writing how much time we search
     logger = get_custom_logger()
-    logger.info('Time for search by word "{0}" is {1}'.format(search_text, end_time))
+    logger.info('Time for search by word "{0}" is {1}'.format(searching_text, end_time))
 
     send_email(result_search, email)
     return result_search
@@ -148,14 +131,14 @@ def send_email(message_string, email):
     :param email: on what email
     :return: none
     """
-    with mail.connect() as conn:  # get server connection
-        subject = "Search result in books"
-        # create message for sending
-        msg = Message(recipients=[email],
-                      body=message_string,
-                      subject=subject,
-                      sender=MAIL_USERNAME)
-        conn.send(msg)
+    # with mail.connect() as conn:  # get server connection
+    subject = "Search result in books"
+    # create message for sending
+    msg = Message(recipients=[email],
+                  body=message_string,
+                  subject=subject,
+                  sender=app.config['MAIL_USERNAME'])
+    mail.send(msg)
 
 
 @app.route('/result', methods=['GET'])
@@ -175,7 +158,7 @@ def get_custom_logger():
     """
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler(LOG_FILE, 'w')
+    handler = logging.FileHandler(app.config['LOG_FILE'], 'w')
     logger.addHandler(handler)
     return logger
 
@@ -186,11 +169,9 @@ def page_not_found(e):
 
 
 @app.errorhandler(500)
-@app.route('/err')
 def server_error(e):
     return render_template('TestBooks/error500.html'), 500
 
 
 if __name__ == '__main__':
-    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-    app.run()
+    app.run(host='0.0.0.0')
